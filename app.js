5ab9b52b0b798c9f0d47199e8de763111a7e1526
Cristopheryuri19 <bassualdoyuri@gmail.com>
2025-01-22 16:44:35 -0300

    aula 22-01

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-import carros2025 from "./tabelaCarros";
+import carros2025 from "./tabelaCarros.js";
 
 
 import express from "express";
@@ -7,6 +7,9 @@ const app = express();
 
 app.use(express.json());
 
+app.get('/', (requisicao, resposta) => {
+    resposta.status(200).send(carros2025); // Retorna lista de carros com status 200 
+});
 
 // define a porta do servidor
 app.listen(3000, () => {
