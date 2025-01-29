3af0b7a0d142418f88a72b79b691cc3519a2e345
CRISTOPHER YURI DE SOUZA BASSUALDO <cristopher.ysbassual@senacsp.edu.br>
2025-01-29 17:01:24 -0300

    aula 29-01

--- tabelaCarros.js
+++ tabelaCarros.js
@@ -1,21 +1,17 @@
 const carros2025 = [
     {
         nome: 'Ferrari',
-        sigla: 'Fer',
+        sigla: 'FER',
         velocidadeMaxima: 340,
         potencia: 800,
-        consumo: 5.5,
-        aceleracao: 2.9,
-        preco: 3000000,
+        consumo: 5.5, 
     },
     {
         nome: 'Lamborghini',
-        sigla: 'Lam',
+        sigla: 'LAM',
         velocidadeMaxima: 355,
         potencia: 770,
         consumo: 6.2,
-        aceleracao: 2.8,
-        preco: 5000000,
     },
 ];
 
---
+++ validacao.js
@@ -0,0 +1,35 @@
+import Joi from "joi";
+
+// Validação para o modelo de carro
+export const modeloCarro = Joi.object({
+  nome: Joi.string().min(3).required().messages({
+    "string.min": "O Nome do carro deve ter pelo menos 3 caracteres.",
+    "any.required": "O Nome do carro é obrigatório",
+  }), // nome do carro, pelo menos 3 caracteres
+  sigla: Joi.string().length(3).required().messages({
+    "string.min": "A sigla deve ter exatamente 3 caracteres.",
+    "any.required": "A sigla é obrigatória",
+  }), // Sigla ou medelo, 3 caracteres
+  velocidadeMaxima: Joi.number().min(1).required().messages({
+    "number.min": "A velocidade máxima deve ser maior ou igual a 1.",
+    "any.required": "A velocidade máxima é obrigatória",
+  }), // Potência minima e 1 Cv
+  potencia: Joi.number().min(1).required().messages({
+    "number.min": "A potêmcia deve ser maior ou igual a 1.",
+    "any.required": "A pot~encia é obrigatória",
+  }), // Velocidade minima de 1km/h
+  consumo: Joi.number().min(0.1).required().messages({
+    "number.min": " O consumo deve ser maior ou igual a 0.1.",
+    "any.required": "O consumo é obrigatório.",
+  }), // Ano de fabricação
+});
+
+// Validação para atualização de carro
+
+export const modeloAtualizacao = Joi.object({
+  nome: Joi.string().min(3), // nome do carro, pelo menos 3 caracteres
+  sigla: Joi.string().length(3), // Sigla ou medelo, 3 caracteres
+  velocidadeMaxima: Joi.number().min(1), // Potência minima e 1 Cv
+  potencia: Joi.number().min(1), // Velocidade minima de 1km/h
+  consumo: Joi.number().min(0.1), // Ano de fabricação
+});
